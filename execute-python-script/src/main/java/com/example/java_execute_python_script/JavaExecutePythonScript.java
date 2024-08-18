0a02a7586fad33e6697688c3c16fbaf1b592df02
fulanbin <lanbin.fu>
2024-08-19 00:43:55 +0800

    sample of java executing python script with ScriptEngineManager

---
+++ execute-python-script/src/main/java/com/example/java_execute_python_script/JavaExecutePythonScript.java
@@ -0,0 +1,19 @@
+package com.example.java_execute_python_script;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.io.File;
+import java.io.FileReader;
+
+public class JavaExecutePythonScript {
+    public static void main(String[] args) throws Exception {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("python");
+        System.out.println(new File("").getAbsolutePath());
+        FileReader fr = new FileReader(JavaExecutePythonScript.class.getClassLoader().getResource("python_script/hello_world.py").getFile());
+        engine.put("name", "Sam");
+        // Execute the script
+        engine.eval(fr);
+    }
+}
---
+++ execute-python-script/src/main/resources/python_script/hello_world.py
@@ -0,0 +1,9 @@
+# hello.py
+import sys
+
+def say_hello(name):
+    print("Hello "+ name)
+
+if __name__ == "__main__":
+    name = globals().get('name', None)
+    say_hello(name)
