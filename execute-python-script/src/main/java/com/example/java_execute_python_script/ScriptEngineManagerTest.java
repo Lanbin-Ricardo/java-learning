0d5f2b7d278917cedd29d126291345908c2fd950
fulanbin <lanbin.fu>
2024-08-19 17:39:03 +0800

    rename class

--- execute-python-script/src/main/java/com/example/java_execute_python_script/ScriptEngineManagerTest.java
+++ execute-python-script/src/main/java/com/example/java_execute_python_script/ScriptEngineManagerTest.java
@@ -1,25 +1,20 @@
 package com.example.java_execute_python_script;
 
-import com.example.pojo.Person;
-import org.python.core.PyObject;
-import org.python.util.PythonInterpreter;
-
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class JavaExecutePythonScript {
+public class ScriptEngineManagerTest {
     public static void main(String[] args) throws Exception {
         ScriptEngineManager manager = new ScriptEngineManager();
         ScriptEngine engine = manager.getEngineByName("python");
         System.out.println(new File("").getAbsolutePath());
-        FileReader fr = new FileReader(JavaExecutePythonScript.class.getClassLoader().getResource("python_script/hello_world.py").getFile());
+        FileReader fr = new FileReader(ScriptEngineManagerTest.class.getClassLoader().getResource("python_script/hello_world.py").getFile());
         engine.put("name", "Sam");
         // Execute the script
         engine.put("names", List.of("Jack", "Jane", "Nik"));
